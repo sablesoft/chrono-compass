37cd8ed08b6d91c5cdca76c67e4b9ed557a0bc79
Roman Sobol <sable.lair@gmail.com>
2026-09-21 14:25:32 -0300

    Precache all documentation languages

--- vite.config.ts
+++ vite.config.ts
@@ -17,10 +17,7 @@ export default defineConfig({
         'favicon.svg',
         'apple-touch-icon-180x180.png',
         'robots.txt',
-        'docs/en/calendars/epoch-calendar.md',
-        'docs/en/calendars/gregorian-calendar.md',
-        'docs/ru/calendars/epoch-calendar.md',
-        'docs/ru/calendars/gregorian-calendar.md'
+        'docs/**/*.md'
       ],
 
       manifest: {
